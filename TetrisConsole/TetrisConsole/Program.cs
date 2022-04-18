4666fce1809d7a47d2324d051bbb31e779402dde
BrainX <brain_x@mail.ru>
2022-04-18 21:50:21 +1000

    Add constructor

--- TetrisConsole/TetrisConsole/Program.cs
+++ TetrisConsole/TetrisConsole/Program.cs
@@ -9,10 +9,7 @@ namespace TetrisConsole
             Console.SetWindowSize(40, 30); //размер окна
             Console.SetBufferSize(40, 30); //уменьшаем зону буфера текста, чтобы скрыть полосы прокрутки
 
-            Point p1 = new Point();
-            p1.x = 2;
-            p1.y = 3;
-            p1.c = '*';
+            Point p1 = new Point(2, 3, '*');
             p1.Draw();
 
             Console.ReadLine();
