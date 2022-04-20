ea408dde50cfac4a76207f51d41c12347ec1be7e
BrainX <brain_x@mail.ru>
2022-04-20 23:08:14 +1000

    Add enum

--- TetrisConsole/TetrisConsole/Program.cs
+++ TetrisConsole/TetrisConsole/Program.cs
@@ -9,14 +9,28 @@ namespace TetrisConsole
             Console.SetWindowSize(40, 30); //размер окна
             Console.SetBufferSize(40, 30); //уменьшаем зону буфера текста, чтобы скрыть полосы прокрутки
 
-            Figure[] f = new Figure[2];
-            f[0] = new Square(2, 5, '*');
-            f[1] = new Stick(6, 6, '*');
+            Square s = new Square(2, 5, '*');
+            s.Draw();
 
-            foreach (Figure fig in f)
-            {
-                fig.Draw();
-            }
+            Thread.Sleep(1000);
+            s.Hide();
+            s.Move(Direction.LEFT);
+            s.Draw();
+
+            Thread.Sleep(1000);
+            s.Hide();
+            s.Move(Direction.DOWN);
+            s.Draw();
+
+            Thread.Sleep(1000);
+            s.Hide();
+            s.Move(Direction.DOWN);
+            s.Draw();
+
+            Thread.Sleep(1000);
+            s.Hide();
+            s.Move(Direction.RIGHT);
+            s.Draw();
 
             Console.ReadLine();
         }
